5c19d145ad370fa56e085874db83ad78c205b2c9
Ethan Stark <estark@Ethans-LG-MacBook-Pro.local>
2016-07-27 16:22:33 -0700

    Join function + test

--- src/lib/join.js
+++ src/lib/join.js
@@ -1,8 +1,15 @@
 'use strict'
 
 const join = (arr, sep) => {
-  
-  return arr.join('')
+  let newArr = []
+  for (let i = 0; i < arr.length; ++i) {
+    newArr.push(arr[i])
+    if (i < (arr.length - 1)) {
+      newArr.push(sep)
+    }
+  }
+
+  return newArr.join('')
 }
 
 export { join }
