16cd61140dfcae3a94cdae0cc2533b1edeac9dc0
Majid Rahimi <Majid88Rahimi@gmail.com>
2016-07-27 14:15:15 -0700

    Added takeRight function

---
+++ src/spec/takeRightSpec.js
@@ -0,0 +1,16 @@
+import _ from 'lodash'
+import {takeRight} from '../lib/takeRight.js'
+
+describe("takeRight", () => {
+  it("Creates a slice of array with n elements taken from the end.", () => {
+    expect(takeRight( [1, 2, 3, 4], 2 ) ).toEqual( [3, 4] )
+  })
+
+  it("if you pass zero number it should return a empty array", () => {
+    expect(takeRight( [1, 2, 3, 4], 0 ) ).toEqual( [] )
+  })
+
+  it("if you don't pass any number it should take one number.", () => {
+    expect(takeRight( [1, 2, 3, 4] ) ).toEqual( [4] )
+  })
+})
