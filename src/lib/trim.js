635ce42de1c3580cdafc0fcc677b692c754aa2fc
Devon Wesley <dwesley89@gmail.com>
2016-07-27 20:53:54 -0700

    trim function is working in trim.js

--- src/lib/trim.js
+++ src/lib/trim.js
@@ -3,23 +3,15 @@
 const trim = (str, char) => {
  let arr = str.split('')
  let charArr = char.split('')
- let newArr = []
-
- let m = charArr.map((x, i, newArr) => arr.filter((y) => y !== x ) )
-
- console.log('Whats happening: ' + newArr);
-
- // arr.map( x => {
- //   console.log('x : ', x);
- //   charArr.map( y => {
- //      if(y === x){
- //        const index = arr.indexOf(x)
- //        arr.splice(index, 1)
- //      }
- //   })
- // })
-
- return m.join('')
+ arr.map((x) => {
+   charArr.map((y) => {
+      if(x === y){
+        arr.shift( x )
+        arr.pop( x )
+      }
+   })
+ })
+ return arr.join('')
 }
 
 export { trim }
--- src/spec/trimSpec.js
+++ src/spec/trimSpec.js
@@ -1,16 +1,16 @@
-// import _ from 'lodash'
-// import { trim } from '../lib/trim.js'
-//
-// describe("trim", () => {
-//   const str = '-_-abc-_-'
-//   const char = '_-'
-//
-//   it("Removes leading and trailing whitespace or specified characters from string.", () => {
-//     expect(trim(str, char)).toEqual('abc')
-//   })
-//
-//   it("checks lodashs trim method against ours", () => {
-//     const lodashTrim = _.trim( str, char )
-//     expect( trim( str, char ) ).toEqual( lodashTrim )
-//   })
-// })
+import _ from 'lodash'
+import { trim } from '../lib/trim.js'
+
+describe("trim", () => {
+  const str = '-_-ab-c-_-'
+  const char = '_-'
+
+  it("Removes leading and trailing whitespace or specified characters from string.", () => {
+    expect(trim(str, char)).toEqual('ab-c')
+  })
+
+  it("checks lodashs trim method against ours", () => {
+    const lodashTrim = _.trim( str, char )
+    expect( trim( str, char ) ).toEqual( lodashTrim )
+  })
+})
